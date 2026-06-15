2ff3e12b56e835133dca192877c58fd3764812b4
hadikhelil73-spec <hadikhelil73@gmail.com>
2026-06-15 15:33:03 +0200

    Add personal information and print statement

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,5 @@
-# Écrivez votre code ici !
+nom = "Hadi"
+age = "16" 
+taille = "1.81"
+est_étudiant = "true"
+print (f"Je m'appelle {nom} et j'ai {age} ans et je mesure {taille} metres et je suis étudiant {est_étudiant}.") # Écrivez votre code ici !
